6e7502b885dad041cfde86a1c943490ff716ccf1
ismaouste <ismael.rodmacq@gmail.com>
2023-03-21 17:42:58 +0100

    :hammer: Implementation of filters and login page

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,26 +1,60 @@
 // on page load (in the browser), load works from the backend
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
+  loadWorks();
+  const btnAll = document.querySelector(".btn-all");
+  const btnObjets = document.querySelector(".btn-objets");
+  const btnApps = document.querySelector(".btn-apps");
+  const btnHotel = document.querySelector(".btn-hotel");
+  btnAll.addEventListener("click", function () {
     loadWorks();
+  });
+
+  btnObjets.addEventListener("click", function () {
+    loadWorks(1);
+  });
+
+  btnApps.addEventListener("click", function () {
+    loadWorks(2);
+  });
+
+  btnHotel.addEventListener("click", function () {
+    loadWorks(3);
+  });
 });
 
-function loadWorks() {
-//fetch works from the backend
-  fetch('http://localhost:5678/api/works')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      // and display them in the DOM
-      const gallery = document.querySelector('.gallery');
+function loadWorks(categoryId) {
+  try {
+    //fetch works from the backend
+    fetch("http://localhost:5678/api/works")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // and display them in the DOM
+        const gallery = document.querySelector(".gallery");
+        // check if data.categoryId === categoryId (if categoryId is defined)
+        // if yes, display the work
+        // if no, display all works
+        gallery.innerHTML = "";
 
-      data.forEach((work) => {
-        // add figure element inside div.gallery
-        const figure = document.createElement('figure');
-        figure.innerHTML = `
+        data = data.filter((work) => {
+          if (categoryId) {
+            return work.categoryId === categoryId;
+          } else {
+            return true;
+          }
+        });
+
+        data.forEach((work) => {
+          // add figure element inside div.gallery
+          const figure = document.createElement("figure");
+          figure.innerHTML = `
             <img src="${work.imageUrl}" alt="${work.title}">
             <figcaption>${work.title}</figcaption>
         `;
-        gallery.appendChild(figure);
+          gallery.appendChild(figure);
+        });
       });
-    });
+  } catch {
+    console.log("error");
+  }
 }
-
